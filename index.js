67f56f496fa2f493c8cb322faa73cf599b14b56d
Kasperi Hallinen <kasperi.hallinen@tuni.fi>
2023-02-24 13:08:32 +0200

    Add exercises 3.19-3.21

--- index.js
+++ index.js
@@ -20,40 +20,17 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(people => {
-    res.json(people)
+  Person.find({}).then(persons => {
+    res.json(persons)
   })
 })
 
 app.get('/info', (req, res) => {
-  Person.find({}).then(people => {
+  Person.find({}).then(persons => {
     res.send(
       `<div>
-        <p>Phonebook has info for ${people.length} people</p>
+        <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
       </div>`
     )
@@ -81,22 +58,8 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
-  let errorMessage = ''
-
-  if (!body.name) {
-    errorMessage = 'Name is missing'
-  }
-  else if (!body.number) {
-    errorMessage = 'Number is missing'
-  }
-
-  if (errorMessage) {
-    return res.status(400).json({
-      error: errorMessage
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -106,17 +69,16 @@ app.post('/api/persons', (req, res) => {
   person.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const {name, number} = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+  const person = {name, number}
 
-  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+  Person.findByIdAndUpdate(req.params.id, person, 
+    {new: true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
@@ -128,6 +90,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).send({error: error.message})
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -14,8 +14,20 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: [true, 'Phone number required'],
+    validate: {
+      validator: v => /^[0-9]{2,3}-[0-9]{6,}$/.test(v),
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
